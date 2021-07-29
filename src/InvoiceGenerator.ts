265d5b954e4def3f13a064de5ea001ffeb56e581
Omkar Birade <omkarbirade20@gmail.com>
2021-07-29 13:39:50 +0530

    refactor: extract constants for fare per km, minute, and booking charges

--- src/InvoiceGenerator.ts
+++ src/InvoiceGenerator.ts
@@ -1,6 +1,10 @@
 export class InvoiceGenerator {
+    public static FARE_PER_KM: number = 10;
+    public static FARE_PER_MINUTE: number = 1;
+    public static BOOKING_CHARGES: number = 5;
+
     totalFare(distance: number, duration: number): number {
-        return distance * 10 + duration * 1 + 5
+        return distance * InvoiceGenerator.FARE_PER_KM + duration * InvoiceGenerator.FARE_PER_MINUTE + InvoiceGenerator.BOOKING_CHARGES
     }
 
 }
