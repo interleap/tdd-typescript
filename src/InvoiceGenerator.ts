f6a7e56589f094004246091e06cdfe72f27522ba
Omkar Birade <omkarbirade20@gmail.com>
2021-07-29 14:13:14 +0530

    refactor: renamed invoice generator's primary method to generate

--- src/InvoiceGenerator.ts
+++ src/InvoiceGenerator.ts
@@ -2,7 +2,7 @@ import { Invoice } from "./Invoice";
 import { Ride } from "./Ride";
 
 export class InvoiceGenerator {
-    totalFare(rides: Array<Ride>): Invoice {
+    generate(rides: Array<Ride>): Invoice {
         const totalFare = rides.reduce((sum: number, ride: Ride) => sum + ride.fare(), 0)
         const numberOfRides = rides.length
         const averageFarePerRide = totalFare / numberOfRides
--- test/src/InvoiceGenerator.test.ts
+++ test/src/InvoiceGenerator.test.ts
@@ -6,7 +6,7 @@ describe("Invoice Generator Test", () => {
     const invoiceGenerator = new InvoiceGenerator()
     it("calculate the total fare given multiple rides", () => {
         const rides = [new Ride(10.0, 7.0), new Ride(3.0, 10.0)]
-        const invoice = invoiceGenerator.totalFare(rides)
+        const invoice = invoiceGenerator.generate(rides)
         const expectedInvoice = new Invoice(157.0, 2, 78.5)
         expect(invoice).toStrictEqual(expectedInvoice)
     })
